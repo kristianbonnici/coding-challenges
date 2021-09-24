dd5a634c9132a60e215dc709d4af21364eb867e3
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-09-24 17:04:42 +0200

    Initial commit

---
+++ data-structures/array/contains_duplicate.py
@@ -0,0 +1,22 @@
+"""
+TASK: Given an integer array nums, return true if any value appears at least twice in the array,
+      and return false if every element is distinct.
+
+DIFFICULTY: Easy
+
+(leetCode) Submission 09/24/2021 16:09
+    Runtime: 96 ms (faster than 82.79% of Python submissions)
+    Memory Usage: 19.1 MB (less than 56.26% of Python submissions)
+"""
+
+
+class Solution(object):
+    def contains_duplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
