0bb1125e9d0eb45d2bc565b601ade343acc7c628
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-09-24 17:12:47 +0200

    Update task description

--- data-structures/array/contains_duplicate.py
+++ data-structures/array/contains_duplicate.py
@@ -2,6 +2,14 @@
 TASK: Given an integer array nums, return true if any value appears at least twice in the array,
       and return false if every element is distinct.
 
+EXAMPLES:
+    Example 1:
+        Input: nums = [1,2,3,1]
+        Output: true
+    Example 2:
+        Input: nums = [1,2,3,4]
+        Output: false
+
 DIFFICULTY: Easy
 
 (leetCode) Submission 09/24/2021 16:09
